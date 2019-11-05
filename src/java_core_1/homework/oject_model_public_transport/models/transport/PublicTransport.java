b686efe24c397ccfc6b5009c6b453f71ff583381
Yauhen_Berazouski <berezovskievgenisergeevich@mail.ru>
2019-11-05 16:38:48 +0300

    add polimorfism to Transport

--- src/java_core_1/homework/oject_model_public_transport/models/transport/PublicTransport.java
+++ src/java_core_1/homework/oject_model_public_transport/models/transport/PublicTransport.java
@@ -1,6 +1,6 @@
 package java_core_1.homework.oject_model_public_transport.models.transport;
 
-public abstract class PublicTransport implements Transport{
+public abstract class PublicTransport implements Transport {
     private String name;
     private String cost;
     private String fuelConsumption;
@@ -9,6 +9,11 @@ public abstract class PublicTransport implements Transport{
         this.name = name;
         this.cost = cost;
         this.fuelConsumption = fuelConsumption;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("PublicTransport run");
     }
 
     @Override
