ede29fc3ef1bc21d42e2de512a1d30c1e358f673
Yauhen_Berazouski <berezovskievgenisergeevich@mail.ru>
2019-11-18 16:53:28 +0300

    Task 8. Magic Tree

--- src/com/epam/javaCore3/homework/task8/Runner.java
+++ src/com/epam/javaCore3/homework/task8/Runner.java
@@ -5,13 +5,30 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
+        List<Tree> childred2 = new ArrayList<>();
+        childred2.add(new Tree(11, new ArrayList<>()));
+        childred2.add(new Tree(12, new ArrayList<>()));
+
+        List<Tree> childred3 = new ArrayList<>();
+        childred3.add(new Tree(21, new ArrayList<>()));
+        childred3.add(new Tree(22, new ArrayList<>()));
 
         List<Tree> children = new ArrayList<>();
 
-        children.add(new Tree(1, new ArrayList<>()));
-        children.add(new Tree(2, new ArrayList<>()));
+        children.add(new Tree(1, childred2));
 
-        Tree tree = new Tree(0, (Tree) null);
-        System.out.println(tree);
+
+        children.add(new Tree(2, childred3));
+
+        Tree tree = new Tree(0, children);
+        tree.flattened().forEach(System.out::println);
+
+        System.out.println(tree.getAllValues());
+        System.out.println(tree.getEvenValues());
+        System.out.println(tree.sumOfEvenValues());
+        System.out.println(tree.isContains13());
+
+
     }
+
 }
--- src/com/epam/javaCore3/homework/task8/Tree.java
+++ src/com/epam/javaCore3/homework/task8/Tree.java
@@ -3,6 +3,8 @@ package com.epam.javaCore3.homework.task8;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
@@ -30,6 +32,33 @@ public class Tree {
     }
 
     public Stream<Tree> flattened() {
-        return children.stream();
+
+        return this.getChildren().stream().map(Tree::flattened)
+                .reduce(Stream.of(this), Stream::concat);
+    }
+
+    public List<Integer> getAllValues() {
+        return flattened().map(tree -> tree.getValue()).collect(Collectors.toList());
+    }
+
+    public List<Integer> getEvenValues() {
+        return getAllValues().stream().filter(value -> value % 2 == 0).collect(Collectors.toList());
+
+    }
+
+    // Sum of even values:
+    public Optional<Integer> sumOfEvenValues() {
+        return Optional.of(getAllValues().stream().mapToInt(integer -> integer.intValue()).sum());
+    }
+
+    public boolean isContains13() {
+        return getAllValues().stream().anyMatch(integer -> integer == 13);//filter(integer -> integer == 13).count() > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "value=" + value +
+                '}';
     }
 }
