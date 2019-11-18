7a870c83ac018f725b8224aa0238172dc54da7c7
Yauhen_Berazouski <berezovskievgenisergeevich@mail.ru>
2019-11-18 15:17:10 +0300

    remove wf-mentoring-program.iml

--- src/com/epam/javaCore3/homework/task8/Runner.java
+++ src/com/epam/javaCore3/homework/task8/Runner.java
@@ -1,4 +1,17 @@
 package com.epam.javaCore3.homework.task8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
+    public static void main(String[] args) {
+
+        List<Tree> children = new ArrayList<>();
+
+        children.add(new Tree(1, new ArrayList<>()));
+        children.add(new Tree(2, new ArrayList<>()));
+
+        Tree tree = new Tree(0, (Tree) null);
+        System.out.println(tree);
+    }
 }
--- src/com/epam/javaCore3/homework/task8/Tree.java
+++ src/com/epam/javaCore3/homework/task8/Tree.java
@@ -1,4 +1,35 @@
 package com.epam.javaCore3.homework.task8;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+
 public class Tree {
+    private int value;
+    private List<Tree> children = new LinkedList<>();
+
+    public Tree(int value, List<Tree> children) {
+        super();
+        this.value = value;
+        this.children.addAll(children);
+    }
+
+    public Tree(int value, Tree... children) {
+        this(value, asList(children));
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public List<Tree> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    public Stream<Tree> flattened() {
+        return children.stream();
+    }
 }
